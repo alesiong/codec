f0d825b2e4220140cd85853a6770b495cf3160a0
alesiong <ypf7.10@hotmail.com>
2019-12-01 22:22:01 +0800

    sort codec list

--- codecs/codec.go
+++ codecs/codec.go
@@ -2,7 +2,6 @@ package codecs
 
 import (
 	"io"
-	"sync"
 )
 
 type CodecMode int
@@ -26,35 +25,6 @@ type CodecUsage interface {
 
 type CodecMetaInfo struct {
 	codecsMap map[string]Codec
-}
-
-func (c *CodecMetaInfo) Register(name string, codec Codec) {
-	c.codecsMap[name] = codec
-}
-
-func (c *CodecMetaInfo) Lookup(name string) Codec {
-	return c.codecsMap[name]
-}
-
-func (c *CodecMetaInfo) AllCodecs() (chan string, chan Codec) {
-	codecChan := make(chan Codec)
-	nameChan := make(chan string)
-	var wg sync.WaitGroup
-
-	for name, codec := range c.codecsMap {
-		go func(name string, codec Codec) {
-			nameChan <- name
-			codecChan <- codec
-			wg.Done()
-		}(name, codec)
-		wg.Add(1)
-	}
-	go func() {
-		wg.Wait()
-		close(nameChan)
-		close(codecChan)
-	}()
-	return nameChan, codecChan
 }
 
 func Register(name string, codec Codec) {
--- codecs/meta.go
+++ codecs/meta.go
@@ -1,6 +1,9 @@
 package codecs
 
-import "io"
+import (
+	"io"
+	"sort"
+)
 
 type MetaCodec interface {
 	RunMetaCodec(input io.Reader, globalMode CodecMode, options map[string]string, metaInfo *CodecMetaInfo, output io.Writer) (err error)
@@ -16,4 +19,34 @@ func (m metaCodecWrap) RunCodec(input io.Reader, globalMode CodecMode, options m
 
 func RegisterMeta(name string, metaCodec MetaCodec) {
 	Register(name, metaCodecWrap{metaCodec})
+}
+
+func (c *CodecMetaInfo) Register(name string, codec Codec) {
+	c.codecsMap[name] = codec
+}
+
+func (c *CodecMetaInfo) Lookup(name string) Codec {
+	return c.codecsMap[name]
+}
+
+func (c *CodecMetaInfo) AllCodecs() (chan string, chan Codec) {
+	codecChan := make(chan Codec)
+	nameChan := make(chan string)
+
+	keys := make([]string, 0, len(c.codecsMap))
+	for name := range c.codecsMap {
+		keys = append(keys, name)
+	}
+	sort.Strings(keys)
+
+	go func() {
+		for _, name := range keys {
+			nameChan <- name
+			codecChan <- c.codecsMap[name]
+		}
+		close(nameChan)
+		close(codecChan)
+	}()
+
+	return nameChan, codecChan
 }
